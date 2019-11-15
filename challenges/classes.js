5c7f1b63697c79393f4aa410164696461809a67d
nicbongo <nicbongo@protonmail.com>
2019-11-15 15:59:31 -0400

    MVP + class stretch?

--- challenges/classes.js
+++ challenges/classes.js
@@ -28,17 +28,30 @@ class CuboidMaker{
       height: 5
   })
   
-  // Test your volume and surfaceArea methods by uncommenting the logs below:
-  console.log(cuboid.volume()); // 100
-  console.log(cuboid.surfaceArea()); // 130
-  
-  
-  
-
-
-
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
 
-// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
+
+// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  
+// Find out the formulas for volume and surface area for cubes and create those methods 
+// using the dimension properties from CuboidMaker.  
+// Test your work by logging out your volume and surface area.
+
+class CubeMaker extends CuboidMaker{
+    constructor(dimensions){
+        super(dimensions);
+    }
+    // no further methods needed as formula for cuboids and cube (being a cuiboid), 
+    // are the same.
+}
+
+
+let cube = new CubeMaker({
+    length: 5,
+    width: 5,
+    height: 5
+})
+
+console.log(cube.volume()); // 125
+console.log(cube.surfaceArea()); // 150
