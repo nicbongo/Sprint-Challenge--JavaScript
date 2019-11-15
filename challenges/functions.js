c8599fd7711c635a43e80c4004c0e4ebf3647d16
nicbongo <nicbongo@protonmail.com>
2019-11-15 14:41:33 -0400

    function & scope

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,10 @@
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
+consume = function (a, b, cb) {
+  return cb (a, b)
+}
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,16 +18,25 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+let add = (num1, num2) => num1 + num2
+let multiply = (num1, num2) => num1 * num2
+let greeting = (firstName, lastName) => `Hello ${firstName} ${lastName}, nice to meet you!`
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why nestedfunction can access the variable internal.
+
+//The const variable "internal" was declared in the same scope as "nestedFunction", 
+//thereby nested function can access this variable due to in heritance wllowing
+//the values to be passed down. Note that if "internal" was declared in "nestedFunction",
+//scope in the rest of the function would not be able to access it.
+
 
 // Explanation: 
 
